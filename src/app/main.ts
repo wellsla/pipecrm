831610e3b0d0e47426152b3e342109dafd269daf
wellsla <61843654+wellsla@users.noreply.github.com>
2025-10-28 20:25:44 -0300

    :heavy_plus_sign: build: Adding auth0

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createAuth0 } from '@auth0/auth0-vue'
 
 import App from './App.vue'
 import router from '../router'
@@ -15,6 +16,13 @@ const bootstrap = async () => {
   app.use(createPinia())
   app.use(router)
   app.use(PrimeVue, {ripple: true})
+  app.use(createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  }))
   app.mount('#app')
 }
 bootstrap()
